97724d1f92f7425a732fe0fb643ae3dc12ed439b
Nick <nicolas.cinera@gmail.com>
2021-11-06 17:56:14 -0400

    Updated Userbase

--- input_data.py
+++ input_data.py
@@ -3,8 +3,11 @@ import json
 
 with open('user_datajson.json') as file:
     data = json.load(file)
-    for entry in data:
+    for entry in data[:100]:
         new_user = User(name=entry['first_name'], last=entry['last_name'],
                         email=entry['email'], budget=entry['budget'])
         db.session.add(new_user)
     db.session.commit()
+
+# User.query.delete()
+# db.session.commit()
